fb610e0f5eec4023acca39f6711d67b635d56d66
yatimoha <gryazev.tim@yandex.ru>
2023-10-19 21:55:00 +0400

    Больше подробностей (часть 1)

--- src/components/film-list/film-list.tsx
+++ src/components/film-list/film-list.tsx
@@ -1,6 +1,5 @@
 import FilmCard from '../film-card/film-card.tsx';
 import { FilmData, FilmsData } from '../../types/film-data.ts';
-import { useState } from 'react';
 
 type FilmListProps = {
   filmsData: FilmsData;
@@ -8,28 +7,22 @@ type FilmListProps = {
   genre?: string;
   clickHandler?: (item: FilmData) => void;
 }
-const FilmList = ({filmsData, maxCards, genre, clickHandler}: FilmListProps): JSX.Element => {
-  const [activeCardFilm, setActiveCardFilm] = useState(filmsData[0]);
-  const handleMouseOver = (film: FilmData) => {
-    setActiveCardFilm(film);
-  };
-  return (
-    <div className="catalog__films-list">
-      {
-        filmsData
-          .filter((item) => genre ? item.genre.toLowerCase() === genre?.toLowerCase() : item)
-          .filter((item, index) => maxCards ? index < maxCards : item)
-          .map((item): JSX.Element => (
-            <FilmCard
-              key={item.id}
-              film={item}
-              clickHandler={() => clickHandler ? clickHandler(item) : ''}
-              mouseOverHandler={handleMouseOver}
-            />
-          ))
-      }
-    </div>
-  );
-};
+
+const FilmList = ({filmsData, maxCards, genre, clickHandler}: FilmListProps): JSX.Element => (
+  <div className="catalog__films-list">
+    {
+      filmsData
+        .filter((item) => genre ? item.genre.toLowerCase() === genre?.toLowerCase() : item)
+        .filter((item, index) => maxCards ? index < maxCards : item)
+        .map((item): JSX.Element => (
+          <FilmCard
+            key={item.id}
+            film={item}
+            clickHandler={() => clickHandler ? clickHandler(item) : ''}
+          />
+        ))
+    }
+  </div>
+);
 
 export default FilmList;
